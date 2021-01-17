406af0ad77fe2e72e21a691c23e8fc31ab9b136d
liuwenbo <liuwenbo6688@126.com>
2021-01-17 22:31:51 +0800

    commit

--- stream-demo/src/main/java/com/datax/stream/operatorState/TestOperatorStateAndCheckpoint.java
+++ stream-demo/src/main/java/com/datax/stream/operatorState/TestOperatorStateAndCheckpoint.java
@@ -10,15 +10,32 @@ import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
+import org.apache.flink.util.Collector;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
- *
+ * 想知道两次事件1之间，一共发生多少次其他事件，分别是什么事件
+ * <p>
+ * 事件流：1 2 3 4 5 1 3 4 5 6 7 1 4 5 3 9 9 2 1...
+ * 输出：
+ * (4, 2 3 4 5)
+ * (5, 3 4 5 6 7)
+ * (6, 4 5 3 9 9 2)
  */
 public class TestOperatorStateAndCheckpoint {
-
-
 
 
     public static void main(String[] args) throws Exception {
@@ -48,7 +65,6 @@ public class TestOperatorStateAndCheckpoint {
         // RETAIN_ON_CANCELLATION 当作业取消时保留检查点
         checkpointConfig.enableExternalizedCheckpoints(
                 CheckpointConfig.ExternalizedCheckpointCleanup.DELETE_ON_CANCELLATION);
-
 
         /**
          *
@@ -95,8 +111,8 @@ public class TestOperatorStateAndCheckpoint {
         // 固定延迟
         env.setRestartStrategy(
                 RestartStrategies.fixedDelayRestart(3, // number of restart attempts
-                                                     Time.of(10, TimeUnit.SECONDS) // delay
-        ));
+                        Time.of(10, TimeUnit.SECONDS) // delay
+                ));
 
 
         // 延迟率
@@ -109,6 +125,9 @@ public class TestOperatorStateAndCheckpoint {
 
         DataStream<Long> inputStream = env.fromElements(1L, 2L, 3L, 4L, 5L, 1L, 3L, 4L, 5L, 6L, 7L, 1L, 4L, 5L, 3L, 9L, 9L, 2L, 1L);
 
+        /**
+         *
+         */
         inputStream.flatMap(new CountWithOperatorState())
                 // 并行度修改，会让这个实例看着很怪，就是因为状态是针对一个operator
                 .setParallelism(1)
@@ -116,5 +135,84 @@ public class TestOperatorStateAndCheckpoint {
 
         env.execute();
 
+    }
+}
+
+
+class CountWithOperatorState extends RichFlatMapFunction<Long, Tuple2<Integer, String>>
+        /**
+         * 实现 CheckpointedFunction 接口
+         */
+        implements CheckpointedFunction {
+
+
+    /**
+     * OperatorState
+     * 只支持 ListState 类型
+     */
+    private transient ListState<Long> checkPointCountList;
+
+    /**
+     * 原始状态
+     */
+    private List<Long> listBufferElements;
+
+    @Override
+    public void flatMap(Long value, Collector<Tuple2<Integer, String>> out) throws Exception {
+        if (value == 1) {
+            // 接收到的值1时就计算并输出
+            if (listBufferElements.size() > 0) {
+                StringBuffer buffer = new StringBuffer();
+                for (Long item : listBufferElements) {
+                    buffer.append(item + " ");
+                }
+                out.collect(Tuple2.of(listBufferElements.size(), buffer.toString()));
+                listBufferElements.clear();
+            }
+        } else {
+            listBufferElements.add(value);
+        }
+    }
+
+    @Override
+    public void snapshotState(FunctionSnapshotContext context) throws Exception {
+        checkPointCountList.clear();
+        for (Long item : listBufferElements) {
+            checkPointCountList.add(item);
+        }
+    }
+
+
+    /**
+     * 初始化
+     */
+    @Override
+    public void initializeState(FunctionInitializationContext context) throws Exception {
+        /**
+         * 初始化状态
+         */
+        ListStateDescriptor<Long> listStateDescriptor =
+                new ListStateDescriptor<Long>("checkPointCountList", TypeInformation.of(new TypeHint<Long>() {
+                }));
+        /**
+         * getListState
+         * getUnionListState
+         * getBroadcastState
+         */
+        checkPointCountList = context.getOperatorStateStore().getListState(listStateDescriptor);
+
+        if (context.isRestored()) {
+            // isRestored代表是挂了重新启动的
+            // 恢复数据到原始状态里
+            for (Long element : checkPointCountList.get()) {
+                listBufferElements.add(element);
+            }
+        }
+    }
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        listBufferElements = new ArrayList<>();
     }
 }
