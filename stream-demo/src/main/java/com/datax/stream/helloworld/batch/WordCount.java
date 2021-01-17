bce95c5fae3ee5929039b78fae099a17f91ebc9a
liuwenbo <liuwenbo6688@126.com>
2021-01-17 08:46:58 +0800

    commit

--- stream-demo/src/main/java/com/datax/stream/helloworld/batch/WordCount.java
+++ stream-demo/src/main/java/com/datax/stream/helloworld/batch/WordCount.java
@@ -38,10 +38,16 @@ public class WordCount {
     }
 
     public static final class Tokenizer extends RichFlatMapFunction<String, Tuple2<String, Integer>> {
+        /**
+         * 累加器
+         */
         private IntCounter numLines = new IntCounter();
 
         @Override
         public void open(Configuration parameters) throws Exception {
+            /**
+             * 注册累加器
+             */
             getRuntimeContext().addAccumulator("num-lines", this.numLines);
             super.open(parameters);
         }
@@ -53,7 +59,11 @@ public class WordCount {
 
         @Override
         public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            /**
+             * 使用累加器
+             */
             this.numLines.add(1);
+
             String[] tokens = value.toLowerCase().split("\\W+");
             for (String token : tokens) {
                 if (token.length() > 0) {
