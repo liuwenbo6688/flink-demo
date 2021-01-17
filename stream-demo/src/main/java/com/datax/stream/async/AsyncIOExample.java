a62835105bca728667379b39426530989469e1a5
liuwenbo <liuwenbo6688@126.com>
2021-01-17 20:58:27 +0800

    commit

--- stream-demo/src/main/java/com/datax/stream/async/AsyncIOExample.java
+++ stream-demo/src/main/java/com/datax/stream/async/AsyncIOExample.java
@@ -37,9 +37,6 @@ public class AsyncIOExample {
 
     private static final Logger LOG = LoggerFactory.getLogger(AsyncIOExample.class);
 
-    private static final String EXACTLY_ONCE_MODE = "exactly_once";
-    private static final String EVENT_TIME = "EventTime";
-    private static final String INGESTION_TIME = "IngestionTime";
     private static final String ORDERED = "ordered";
 
     /**
@@ -94,28 +91,13 @@ public class AsyncIOExample {
     }
 
 
-    /**
-     * An sample of {@link AsyncFunction} using a thread pool and executing working threads
-     * to simulate multiple async operations.
-     *
-     * <p>For the real use case in production environment, the thread pool may stay in the
-     * async client.
-     */
+
     private static class SampleAsyncFunction extends RichAsyncFunction<Integer, String> {
         private static final long serialVersionUID = 2098635244857937717L;
 
         private transient ExecutorService executorService;
 
-        /**
-         * The result of multiplying sleepFactor with a random float is used to pause
-         * the working thread in the thread pool, simulating a time consuming async operation.
-         */
         private final long sleepFactor;
-
-        /**
-         * The ratio to generate an exception to simulate an async error. For example, the error
-         * may be a TimeoutException while visiting HBase.
-         */
         private final float failRatio;
 
         private final long shutdownWaitTS;
@@ -129,7 +111,6 @@ public class AsyncIOExample {
         @Override
         public void open(Configuration parameters) throws Exception {
             super.open(parameters);
-
             executorService = Executors.newFixedThreadPool(30);
         }
 
@@ -166,122 +147,51 @@ public class AsyncIOExample {
         }
     }
 
-    private static void printUsage() {
-        System.out.println("To customize example, use: AsyncIOExample [--fsStatePath <path to fs state>] " +
-                "[--checkpointMode <exactly_once or at_least_once>] " +
-                "[--maxCount <max number of input from source, -1 for infinite input>] " +
-                "[--sleepFactor <interval to sleep for each stream element>] [--failRatio <possibility to throw exception>] " +
-                "[--waitMode <ordered or unordered>] [--waitOperatorParallelism <parallelism for async wait operator>] " +
-                "[--eventType <EventTime or IngestionTime>] [--shutdownWaitTS <milli sec to wait for thread pool>]" +
-                "[--timeout <Timeout for the asynchronous operations>]");
-    }
 
     public static void main(String[] args) throws Exception {
 
-        // obtain execution environment
+
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.enableCheckpointing(1000L, CheckpointingMode.EXACTLY_ONCE);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
 
-        // parse parameters
-        final ParameterTool params = ParameterTool.fromArgs(args);
-
-        final String statePath;
-        final String cpMode;
-        final int maxCount;
-        final long sleepFactor;
-        final float failRatio;
-        final String mode;
-        final int taskNum;
-        final String timeType;
-        final long shutdownWaitTS;
-        final long timeout;
-
-        try {
-            // check the configuration for the job
-            statePath = params.get("fsStatePath", null);
-            cpMode = params.get("checkpointMode", "exactly_once");
-            maxCount = params.getInt("maxCount", 100000);
-            sleepFactor = params.getLong("sleepFactor", 100);
-            failRatio = params.getFloat("failRatio", 0.001f);
-            mode = params.get("waitMode", "ordered");
-            taskNum = params.getInt("waitOperatorParallelism", 1);
-            timeType = params.get("eventType", "EventTime");
-            shutdownWaitTS = params.getLong("shutdownWaitTS", 20000);
-            timeout = params.getLong("timeout", 10000L);
-        } catch (Exception e) {
-            printUsage();
-
-            throw e;
-        }
-
-        StringBuilder configStringBuilder = new StringBuilder();
-
-        final String lineSeparator = System.getProperty("line.separator");
-
-        configStringBuilder
-                .append("Job configuration").append(lineSeparator)
-                .append("FS state path=").append(statePath).append(lineSeparator)
-                .append("Checkpoint mode=").append(cpMode).append(lineSeparator)
-                .append("Max count of input from source=").append(maxCount).append(lineSeparator)
-                .append("Sleep factor=").append(sleepFactor).append(lineSeparator)
-                .append("Fail ratio=").append(failRatio).append(lineSeparator)
-                .append("Waiting mode=").append(mode).append(lineSeparator)
-                .append("Parallelism for async wait operator=").append(taskNum).append(lineSeparator)
-                .append("Event type=").append(timeType).append(lineSeparator)
-                .append("Shutdown wait timestamp=").append(shutdownWaitTS);
-
-        LOG.info(configStringBuilder.toString());
-
-        if (statePath != null) {
-            // setup state and checkpoint mode
-            env.setStateBackend(new FsStateBackend(statePath));
-        }
-
-        if (EXACTLY_ONCE_MODE.equals(cpMode)) {
-            env.enableCheckpointing(1000L, CheckpointingMode.EXACTLY_ONCE);
-        } else {
-            env.enableCheckpointing(1000L, CheckpointingMode.AT_LEAST_ONCE);
-        }
-
-        // enable watermark or not
-        if (EVENT_TIME.equals(timeType)) {
-            env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-        } else if (INGESTION_TIME.equals(timeType)) {
-            env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
-        }
-
+        final String mode = "ordered";
         // create input stream of an single integer
-        DataStream<Integer> inputStream = env.addSource(new SimpleSource(maxCount));
+        DataStream<Integer> inputStream = env
+                .addSource(new SimpleSource(100000));
 
         // create async function, which will *wait* for a while to simulate the process of async i/o
         AsyncFunction<Integer, String> function =
-                new SampleAsyncFunction(sleepFactor, failRatio, shutdownWaitTS);
+                new SampleAsyncFunction(100, 0.001f, 20000);
 
         // add async operator to streaming job
         DataStream<String> result;
         if (ORDERED.equals(mode)) {
-            // 有序
+            // 有序，消息接收和的发送到下游的顺序相同
             result = AsyncDataStream.orderedWait(
                     inputStream,
                     function, // AsyncFunction
-                    timeout,
+                    10000L,
                     TimeUnit.MILLISECONDS,
-                    20).setParallelism(taskNum);
+                    20)
+                    .setParallelism(1);
         } else {
-            // 无序
+            /**
+             * 无序
+             * 对于 ProcessingTime 完全无序
+             * 对于 EventTime ，两个watermark之间的数据是无序的
+             */
             result = AsyncDataStream.unorderedWait(
                     inputStream,
                     function,// AsyncFunction
-                    timeout,
+                    10000L,
                     TimeUnit.MILLISECONDS,
-                    20).setParallelism(taskNum);
+                    20).setParallelism(1);
         }
 
         // add a reduce to get the sum of each keys.
-        result
-                .flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
-
+        result.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
                     private static final long serialVersionUID = -938116068682344455L;
-
                     @Override
                     public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
                         out.collect(new Tuple2<>(value, 1));
