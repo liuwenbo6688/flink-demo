e78ad7ed472a8b02985416b47808981252456495
liuwb9 <liuwenbo6688@126.com>
2021-02-06 14:03:57 +0800

    commit

--- stream-demo/src/main/java/com/datax/stream/distinct/BitMapDistinct.java
+++ stream-demo/src/main/java/com/datax/stream/distinct/BitMapDistinct.java
@@ -1,34 +1,89 @@
 package com.datax.stream.distinct;
 
+
 import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.roaringbitmap.longlong.Roaring64NavigableMap;
+
 
 /**
  * 海量数据去重方案
+ * <p>
+ * 计算每个APP的去重用户数
  */
-public class BitMapDistinct implements AggregateFunction<Long, Roaring64NavigableMap,Long> {
+public class BitMapDistinct {
+
+
+    /**
+     * APP -> 对应的用户ID
+     */
+    public static final Tuple2[] APP_ID_MAPPING = new Tuple2[]{
+            Tuple2.of("app1", 1001L),
+            Tuple2.of("app2", 1002L),
+            Tuple2.of("app1", 1003L),
+            Tuple2.of("app2", 1004L),
+            Tuple2.of("app1", 1005L),
+            Tuple2.of("app2", 1006L),
+            Tuple2.of("app1", 1001L),
+            Tuple2.of("app2", 1002L),
+            Tuple2.of("app1", 1003L),
+            Tuple2.of("app2", 1004L),
+            Tuple2.of("app1", 1005L),
+            Tuple2.of("app2", 1006L)
+    };
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Tuple2<String, Long>> input = env.fromElements(APP_ID_MAPPING);
+
+
+        DataStream<Tuple2<String, Long>> distinctDS = input
+                .keyBy(0)
+                .countWindow(3) //模拟3个输出一次
+                .aggregate(new BitMapDistinctAggregateFunction());
+
+        distinctDS.print();
+
+        env.execute();
+    }
+
+
+}
+
+
+class BitMapDistinctAggregateFunction implements AggregateFunction<Tuple2<String, Long>,
+        Tuple2<String, Roaring64NavigableMap>, Tuple2<String, Long>> {
 
 
     @Override
-    public Roaring64NavigableMap createAccumulator() {
-        return new Roaring64NavigableMap();
+    public Tuple2<String, Roaring64NavigableMap> createAccumulator() {
+        return new Tuple2<>("", new Roaring64NavigableMap());
     }
 
     @Override
-    public Roaring64NavigableMap add(Long value, Roaring64NavigableMap accumulator) {
-        accumulator.add(value);
+    public Tuple2<String, Roaring64NavigableMap> add(Tuple2<String, Long> value, Tuple2<String, Roaring64NavigableMap> accumulator) {
+        accumulator.f0 = value.f0;
+        accumulator.f1.add(value.f1);
         return accumulator;
     }
 
 
     @Override
-    public Long getResult(Roaring64NavigableMap accumulator) {
+    public Tuple2<String, Long> getResult(Tuple2<String, Roaring64NavigableMap> accumulator) {
         // 获取结果
-        return accumulator.getLongCardinality();
+        return new Tuple2<>(accumulator.f0, accumulator.f1.getLongCardinality());
     }
 
     @Override
-    public Roaring64NavigableMap merge(Roaring64NavigableMap a, Roaring64NavigableMap b) {
+    public Tuple2<String, Roaring64NavigableMap> merge(Tuple2<String, Roaring64NavigableMap> a,
+                                                       Tuple2<String, Roaring64NavigableMap> b) {
         return null;
     }
 }
+
